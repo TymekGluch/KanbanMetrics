e19b1c46f61e2cf79b30c06a9f6a0eddac181003
Tymoteusz GŁuch <gluchtymoteusz@gmail.com>
2026-05-02 16:04:33 +0200

    fix: PageLayout Default fixes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,9 @@
 import PageLayoutDefault from "@/components/PageLayout/PageLayoutDefault";
 
+export const metadata = {
+  title: "Home Page",
+};
+
 export default function Home() {
   return <PageLayoutDefault.Root>sdfdsdsfds</PageLayoutDefault.Root>;
 }
